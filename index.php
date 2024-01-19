6bd7194c7763b5c476f45adf9b046ae391692805
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-01-19 20:53:41 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 echo "Sulfikar Alijun";
+echo "<br>";
 
 // if else while echo dan lain lain tidak peka huruf besar kecil
 ECHO "Hello World!<br>";
@@ -12,3 +13,11 @@ $color = "red";
 echo "My car is " . $color . "<br>"; // peka
 echo "My house is " . $COLOR . "<br>"; // tidak peka
 echo "My boat is " . $coLOR . "<br>"; // tidak peka
+
+// ini adalah single line comment
+
+# ini juga adalah single line comment // tidak direkomendasikan menggunakan hash '#' untuk comment
+
+/*
+ini adalah multi line comment
+*/
